a4aad22cca6281334f870faed49ff3d2e7ed80c9
thanhhaile <thanhhai.bku@gmail.com>
2019-09-05 16:48:53 +0700

    Create a React app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+  render(){
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      <div className="App">
+        {/* {console.log(this.state.monsters)} */}
+        <h1> Monsters Rolodex </h1>
+        <SearchBox placeholder={'search monsters'} handleChange={e => this.setState({searchField: e.target.value})}/>
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
