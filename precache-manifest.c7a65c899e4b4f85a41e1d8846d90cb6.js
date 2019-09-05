924acc84ef50c511f9f9e8246da5a0a69cde0e5d
thanhhaile <thanhhai.bku@gmail.com>
2019-09-05 15:35:17 +0700

    Updates

---
+++ precache-manifest.c7a65c899e4b4f85a41e1d8846d90cb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effd7ac1f9923e4e831e4805627e6b59",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05e0506ef1b82f37be78",
+    "url": "/monsters-rolodex/static/css/main.41f36cd2.chunk.css"
+  },
+  {
+    "revision": "f7abcd3b9d99ae6d26ef",
+    "url": "/monsters-rolodex/static/js/2.bffc00c9.chunk.js"
+  },
+  {
+    "revision": "05e0506ef1b82f37be78",
+    "url": "/monsters-rolodex/static/js/main.fc1ef50f.chunk.js"
+  },
+  {
+    "revision": "61b7037003e2b7deb3db",
+    "url": "/monsters-rolodex/static/js/runtime~main.c73c6000.js"
+  }
+]);
