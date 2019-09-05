6cb7411e6fd60400fc471249ac7166d97121a5c2
thanhhaile <thanhhai.bku@gmail.com>
2019-09-05 17:24:46 +0700

    fixing

---
+++ build/precache-manifest.5aed192f2a95e5dd596e6b1be46fb7ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8863f2602b2619b7a667288a3e19998a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0e7699cfdd38127c4700",
+    "url": "/monsters-rolodex/static/css/main.41f36cd2.chunk.css"
+  },
+  {
+    "revision": "f7abcd3b9d99ae6d26ef",
+    "url": "/monsters-rolodex/static/js/2.bffc00c9.chunk.js"
+  },
+  {
+    "revision": "0e7699cfdd38127c4700",
+    "url": "/monsters-rolodex/static/js/main.cddd5e5a.chunk.js"
+  },
+  {
+    "revision": "61b7037003e2b7deb3db",
+    "url": "/monsters-rolodex/static/js/runtime~main.c73c6000.js"
+  }
+]);
